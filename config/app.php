d5e904a8544197944ad011e430f354c1d97a0b56
Ricardo Gottheil <ricardogottheil@gmail.com>
2016-05-22 18:40:38 -0500

    Se modifica el archivo app.php y el index.php

--- config/app.php
+++ config/app.php
@@ -5,5 +5,8 @@ define('APP_PATH', __DIR__ . '/..');
 // Composer
 require_once APP_PATH . '/vendor/autoload.php';
 
+
+require_once 'env.php';
+
 // Base de datos 
-require_once '$database.php';
+require_once 'database.php';
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once '../config/app.php';
+
+echo "Hola mundo";
